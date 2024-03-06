fe54da4e5ea3632e3618c563603e4d57413c9ec6
Shinrael <mircoelena89@gmail.com>
2024-03-06 16:48:59 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+js-array-objects-carousel
+===
+Dato un array di oggetti letterali con:
+ - url dell’immagine
+ - titolo
+ - descrizione
+Creare un carosello.
+### Svolgimento:
+1. 
---
+++ js/script.js
@@ -0,0 +1,39 @@
+const imageContainer = document.querySelector('.image-container');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+
+const images = [
+    {
+        url: 'img/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'img/peru.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'img/chile.jpg',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'img/argentina.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'img/colombia.jpg',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
+
+images.forEach(img => {
+    imageContainer.innerHTML += `<img class="image hidden" src="${img.url}">`
+})
+
